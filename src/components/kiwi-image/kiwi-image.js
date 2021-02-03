eb992f1088bc8cf3975f406050e51e67d52d5155
Anthony Caron <acaron@atb.com>
2021-02-03 14:19:34 -0800

    Added KiwiImage component

---
+++ src/components/kiwi-image/kiwi-image.js
@@ -0,0 +1,16 @@
+import kiwi from './kiwi.jpg'
+import './kiwi-image.scss'
+
+class KiwiImage {
+  render() {
+    const img = document.createElement('img')
+    img.src = kiwi
+    img.alt = 'Kiwi'
+    img.classList.add('kiwi-image')
+
+    const body = document.querySelector('body')
+    body.appendChild(img)
+  }
+}
+
+export default KiwiImage
--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import HelloWorldButton from './components/hello-world-button/hello-world-button'
 import Heading from './components/heading/heading'
+import KiwiImage from './components/kiwi-image/kiwi-image'
 
 const heading = new Heading()
 heading.render()
 const helloWorld = new HelloWorldButton()
 helloWorld.render()
+const kiwiImage = new KiwiImage()
+kiwiImage.render()
