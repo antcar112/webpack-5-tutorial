1ec02d80d742ba77d462e29d4f3a63e356d8564f
Anthony Caron <acaron@atb.com>
2021-02-06 14:56:06 -0800

    Added React

--- src/hello-world.js
+++ src/hello-world.js
@@ -1,8 +1,8 @@
+import React from 'react'
 import HelloWorldButton from './components/hello-world-button/hello-world-button'
 import Heading from './components/heading/heading'
-import { upperFirst } from 'lodash'
 
 const heading = new Heading()
-heading.render(upperFirst('hello world'))
+heading.render('hello world')
 const helloWorld = new HelloWorldButton()
 helloWorld.render()
--- src/kiwi.js
+++ src/kiwi.js
@@ -1,8 +1,8 @@
+import React from 'react'
 import Heading from './components/heading/heading'
 import KiwiImage from './components/kiwi-image/kiwi-image'
-import { upperFirst } from 'lodash'
 
 const heading = new Heading()
-heading.render(upperFirst('kiwi'))
+heading.render('kiwi')
 const kiwiImage = new KiwiImage()
 kiwiImage.render()
